a930d2f4687ba35ffc3f64ea574a7b12689ba040
fhulu <fhulufhelohanyahanya@gmail.com>
2019-08-05 11:04:31 +0200

    Updated index.php page. title name

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
   <meta name="author" content="MRIS" />
 
-<title>Welcome To M-R.i.S</title>
+<title>Welcome To M-R.i.S </title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
